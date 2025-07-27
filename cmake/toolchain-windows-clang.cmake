b1eec77c42dc1121ced80966d2c073ab49374ecb
trauty <trautwein.maximilian04@gmail.com>
2025-07-27 15:53:23 +0200

    went back to cpm

--- cmake/toolchain-windows-clang.cmake
+++ cmake/toolchain-windows-clang.cmake
@@ -3,16 +3,15 @@ set(CMAKE_SYSTEM_NAME Windows)
 set(CMAKE_C_COMPILER clang)
 set(CMAKE_CXX_COMPILER clang++)
 
-set(CMAKE_EXE_LINKER_FLAGS_INIT "-fuse-ld=lld")
-set(CMAKE_SHARED_LINKER_FLAGS_INIT "-fuse-ld=lld")
-set(CMAKE_MODULE_LINKER_FLAGS_INIT "-fuse-ld=lld")
+set(CMAKE_C_FLAGS_INIT "-flto")
+set(CMAKE_CXX_FLAGS_INIT "-stdlib=libc++ -flto")
 
-set(CMAKE_CXX_FLAGS_INIT "-stdlib=libc++")
-set(CMAKE_CXX_STANDARD_LIBRARIES "-lc++ -lc++abi")
+set(CMAKE_EXE_LINKER_FLAGS_INIT "-fuse-ld=lld -flto -static-libc++ -lc++ -lc++abi -lunwind")
+set(CMAKE_SHARED_LINKER_FLAGS_INIT "-fuse-ld=lld -flto -static-libc++ -lc++ -lc++abi -lunwind")
+set(CMAKE_MODULE_LINKER_FLAGS_INIT "-fuse-ld=lld -flto -static-libc++ -lc++ -lc++abi -lunwind")
 
 set(CMAKE_CXX_STANDARD 20)
 set(CMAKE_CXX_STANDARD_REQUIRED ON)
 set(CMAKE_CXX_EXTENSIONS OFF)
 
-#set(CMAKE_EXE_LINKER_FLAGS "-static-libstdc++ -static-libgcc -lwinpthread")
 set(CPACK_GENERATOR "ZIP")
